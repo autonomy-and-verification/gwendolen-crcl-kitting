8452e4074f7ea8f92324faa8179d246c39511a80
Rafael C. Cardoso <rafael_caue@hotmail.com>
2020-09-22 17:55:45 +0100

    Initial commit, plans with empty preconditions at the moment.

---
+++ src/examples/gwendolen/crcl/kitting/KittingEnv.java
@@ -0,0 +1,148 @@
+package gwendolen.crcl.kitting;
+
+import ail.mas.DefaultEnvironment;
+import ail.syntax.Action;
+import ail.syntax.Literal;
+import ail.syntax.NumberTerm;
+import ail.syntax.NumberTermImpl;
+import ail.syntax.Predicate;
+import ail.syntax.Literal;
+import ail.syntax.StringTerm;
+import ail.syntax.StringTermImpl;
+import ail.syntax.Term;
+import ail.syntax.Unifier;
+import ail.util.AILexception;
+import java.util.Random;
+
+
+
+public class KittingEnv extends DefaultEnvironment{
+	static final String logname = "gwendolen.crcl.kitting.KittingEnv";
+	
+	
+	public KittingEnv() {
+		super();
+		
+		System.out.println("Environment started.");
+		
+		Predicate kit = new Predicate("kit");
+		kit.addTerm(new StringTermImpl("s2b2"));
+		kit.addTerm(new NumberTermImpl(2));
+		kit.addTerm(new NumberTermImpl(0));
+		kit.addTerm(new NumberTermImpl(2));
+		addPercept(kit);
+		
+		Predicate kit_quantity = new Predicate("kit_quantity");
+		kit_quantity.addTerm(new StringTermImpl("s2b2"));
+		kit_quantity.addTerm(new NumberTermImpl(0));
+		kit_quantity.addTerm(new NumberTermImpl(0));
+		kit_quantity.addTerm(new NumberTermImpl(0));
+		addPercept(kit_quantity);
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see ail.mas.DefaultEnvironment#executeAction(java.lang.String, ail.syntax.Action)
+	 */
+	public Unifier executeAction(String agName, Action act) throws AILexception {
+		String actionname = act.getFunctor();
+		boolean action_result = false;
+		System.out.println("Environment is simulating action: "+actionname);
+		if (actionname.equals("detach")) {
+			action_result = simulate_action(0.9, 1000);
+			if (!action_result) {
+				System.out.println("Action: "+actionname+" has failed!");
+			}
+		} else if (actionname.equals("take_part")) {
+			action_result = simulate_action(0.9, 1000);
+			if (!action_result) {
+				System.out.println("Action: "+actionname+" has failed!");
+			}
+		} else if (actionname.equals("take_tray")) {
+			action_result = simulate_action(0.9, 1000);
+			if (!action_result) {
+				System.out.println("Action: "+actionname+" has failed!");
+			}
+		} else if (actionname.equals("place_tray")) {
+			action_result = simulate_action(0.9, 1000);
+			if (!action_result) {
+				System.out.println("Action: "+actionname+" has failed!");
+			}
+		} else if (actionname.equals("create_kit")) {
+			action_result = simulate_action(0.9, 1000);
+			if (!action_result) {
+				System.out.println("Action: "+actionname+" has failed!");
+			}
+		} else if (actionname.equals("attach")) {
+			action_result = simulate_action(0.9, 1000);
+			if (!action_result) {
+				System.out.println("Action: "+actionname+" has failed!");
+			}
+		} else if (actionname.equals("place_kit")) {
+			action_result = simulate_action(0.9, 1000);
+			if (!action_result) {
+				System.out.println("Action: "+actionname+" has failed!");
+			}
+		} else if (actionname.equals("place_part")) {
+			action_result = simulate_action(0.9, 1000);
+			if (!action_result) {
+				System.out.println("Action: "+actionname+" has failed!");
+			}
+		} else if (actionname.equals("look_part")) {
+			action_result = simulate_action(0.9, 1000);
+			if (!action_result) {
+				System.out.println("Action: "+actionname+" has failed!");
+			}
+		} else if (actionname.equals("set_grasp")) {
+			action_result = simulate_action(0.9, 1000);
+			if (!action_result) {
+				System.out.println("Action: "+actionname+" has failed!");
+			}
+		} else if (actionname.equals("look_slot")) {
+			action_result = simulate_action(0.9, 1000);
+			if (!action_result) {
+				System.out.println("Action: "+actionname+" has failed!");
+			}
+		} else if (actionname.equals("take_kit")) {
+			action_result = simulate_action(0.9, 1000);
+			if (!action_result) {
+				System.out.println("Action: "+actionname+" has failed!");
+			}
+		}
+		if (!actionname.equals("print")) {
+			if (action_result) {
+				Predicate action_res = new Predicate("action_result");
+				action_res.addTerm(new StringTermImpl(""+false));
+				removePercept(action_res);
+			} else {
+				Predicate action_res = new Predicate("action_result");
+				action_res.addTerm(new StringTermImpl(""+true));
+				removePercept(action_res);
+			}
+			Predicate action_res = new Predicate("action_result");
+			action_res.addTerm(new StringTermImpl(""+action_result));
+			addPercept(action_res);
+		}
+		
+		return super.executeAction(agName, act);
+				
+	}
+	
+	public boolean simulate_action(double prob, int time) {
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return new Random().nextDouble() <= prob;
+	}
+	
+	
+//	@Override
+//	public boolean done() {
+//		return false;
+//	}
+
+}
+
+	
